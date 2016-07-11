a9cb07306cab00d30dae9cc827aac80370155016
Roland Andrei Groza <rolandjitsu@gmail.com>
2016-07-11 14:32:41 +0200

    fix: fix path parsing

--- dist/path.js
+++ dist/path.js
@@ -1,10 +1,11 @@
 "use strict";
+// TODO: move this to a common place
 // CommonJS `module.id` standard,
 // is the path to the current file
 // Check specs http://wiki.commonjs.org/wiki/Modules/1.1
 function dirname(moduleId) {
     if (moduleId === void 0) { moduleId = ''; }
-    return moduleId.replace(moduleId.replace(/^.*[\\\/]/, '') + "/", // File name, platform (Windows/Linux) agnostic
+    return moduleId.replace(moduleId.replace(/^.*[\\\/]/, ''), // File name, platform (Windows/Linux) agnostic
     '');
 }
 exports.dirname = dirname;
--- src/path.ts
+++ src/path.ts
@@ -1,9 +1,10 @@
+// TODO: move this to a common place
 // CommonJS `module.id` standard,
 // is the path to the current file
 // Check specs http://wiki.commonjs.org/wiki/Modules/1.1
 export function dirname(moduleId = '') {
 	return moduleId.replace(
-		`${moduleId.replace(/^.*[\\\/]/, '')}/`, // File name, platform (Windows/Linux) agnostic
+		moduleId.replace(/^.*[\\\/]/, ''), // File name, platform (Windows/Linux) agnostic
 		''
 	);
 }
