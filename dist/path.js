77d4b4c565799cce2c749bb806ec067e06902272
Roland Andrei Groza <rolandjitsu@gmail.com>
2016-07-11 14:30:05 +0200

    chore: add missing dist files

---
+++ dist/path.js
@@ -0,0 +1,12 @@
+"use strict";
+// CommonJS `module.id` standard,
+// is the path to the current file
+// Check specs http://wiki.commonjs.org/wiki/Modules/1.1
+function dirname(moduleId) {
+    if (moduleId === void 0) { moduleId = ''; }
+    return moduleId.replace(moduleId.replace(/^.*[\\\/]/, '') + "/", // File name, platform (Windows/Linux) agnostic
+    '');
+}
+exports.dirname = dirname;
+
+//# sourceMappingURL=path.js.map
