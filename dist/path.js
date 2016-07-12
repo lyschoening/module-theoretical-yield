c48ae5ee34d08bceba4fde679fdc34b787e727c2
Roland Andrei Groza <rolandjitsu@gmail.com>
2016-07-12 11:10:03 +0200

    refactor(path.ts): remove last forward slash

--- dist/path.js
+++ dist/path.js
@@ -3,10 +3,15 @@
 // CommonJS `module.id` standard,
 // is the path to the current file
 // Check specs http://wiki.commonjs.org/wiki/Modules/1.1
-function dirname(moduleId) {
-    if (moduleId === void 0) { moduleId = ''; }
-    return moduleId.replace(moduleId.replace(/^.*[\\\/]/, ''), // File name, platform (Windows/Linux) agnostic
-    '');
+function dirname(path) {
+    if (path === void 0) { path = ''; }
+    var filename = path.replace(/^.*[\\\/]/, ''); // Platform (Windows/Linux) agnostic
+    path = path.replace(filename, '');
+    var index = path.lastIndexOf('/');
+    if (index !== -1) {
+        path = path.substr(0, index);
+    }
+    return path;
 }
 exports.dirname = dirname;
 
