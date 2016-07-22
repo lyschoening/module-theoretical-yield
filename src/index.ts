4bdef11aa9487ad7469e842f65cdff1b739daf2e
Roland Andrei Groza <rolandjitsu@gmail.com>
2016-07-22 15:09:37 +0200

    docs: add some notes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // Everything that can be imported in the platform,
 // should be exported from here.
 // NOTE: NEVER remove this file.
+// Change `'./example.component'` to where ever your component resides (of course within the source folder).
 export * from './example.component';
 import main from './example.component';
 export default main;
